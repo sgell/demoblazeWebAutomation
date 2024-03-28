e27f310de9c1d3b1b6e2f40671679db41f801389
sgell <71590790+sgell@users.noreply.github.com>
2024-03-28 15:58:30 +0200

    Update SendContact.java

--- src/test/java/sendContact.java
+++ src/test/java/sendContact.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class endContact {
+public class SendContact {
     @Test
     public static void contact() throws InterruptedException {
 
